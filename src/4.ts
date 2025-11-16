83c2a6f1ee82bf44cb0260a53e77381fd88172f2
Myshko Alona <myshko.alona@gmail.com>
2025-11-16 16:30:24 +0000

    done hw

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,68 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
+console.log("Створено ключ з signature:", key.getSignature());
+console.log("Двері закриті:", !house["door"]);
+
 house.openDoor(person.getKey());
+
+console.log("Після відкриття дверей - двері відкриті:", house["door"]);
+console.log("Кількість мешканців:", house["tenants"].length);
 
 house.comeIn(person);
 
+console.log("Після входу - кількість мешканців:", house["tenants"].length);
 
 export {};
